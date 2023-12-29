45fceaf0040dd82e305470c913abc661ea3bf433
ElahehShaban <Elaheh.Shabani85@gmail.com>
2023-12-28 20:56:30 -0500

    Functionality of browsing URL is added

--- app.py
+++ app.py
@@ -1,33 +1,74 @@
 
+
 from flask import Flask, request, jsonify, render_template
 import openai
-
+import requests
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import re
 app = Flask(__name__)
 
-# Set your OpenAI API key here. Remember to keep it secure.
-openai.api_key = '' # Replace with your actual API key
+# Replace with your actual OpenAI API key
+openai.api_key =''  # Replace with your actual API key
 
-def get_openai_response(prompt, model="gpt-4-1106-preview", temperature=0.7):
+def get_openai_response(prompt, model="text-davinci-004", temperature=0.7):
     response = openai.ChatCompletion.create(
-        model=model,
-        messages=[{"role": "user", "content": prompt}],
-        temperature=temperature,
-        max_tokens=150
+        model="gpt-4-1106-preview",  # The chat model identifier
+        messages=[
+            {"role": "system", "content": "You are a knowledgeable assistant."},
+            {"role": "user", "content": prompt}
+        ]
     )
     return response.choices[0].message['content'].strip()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+def is_url(string):
+    try:
+        result = urlparse(string)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+def fetch_url_content(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text
+    except requests.RequestException as e:
+        return str(e)
+
+def extract_text_from_html(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    text = ' '.join(soup.stripped_strings)
+    return text
+
+def find_url_in_text(text):
+    # This regex pattern might need adjustments based on the URLs you expect
+    url_pattern = r'https?://[^\s<>"]+|www\.[^\s<>"]+'
+    match = re.search(url_pattern, text)
+    return match.group(0) if match else None
+
 @app.route('/chat', methods=['POST'])
 def chat():
     data = request.json
     user_input = data.get('message')
+
     if not user_input:
         return jsonify({'error': 'No message provided'}), 400
 
-    bot_response = get_openai_response(user_input)
+    extracted_url = find_url_in_text(user_input)
+    if extracted_url:
+        html_content = fetch_url_content(extracted_url)
+        extracted_text = extract_text_from_html(html_content)
+        prompt = f"{user_input.replace(extracted_url, '')} Here is the content: {extracted_text}"
+    else:
+        prompt = user_input
+
+    bot_response = get_openai_response(prompt)
     return jsonify({'response': bot_response})
 
 if __name__ == '__main__':
--- static/script.js
+++ static/script.js
@@ -1,19 +1,25 @@
 async function sendMessage() {
     const userInput = document.getElementById('userInput');
     const chatbox = document.getElementById('chatbox');
-    const message = userInput.value;
+    const message = userInput.value.trim();
     userInput.value = '';
 
-    chatbox.innerHTML += `<div class="message user-message">${message}</div>`;
+    if (message) {
+        const messageHTML = `<div class="message-pair">
+                                <div class="message user-message">${message}</div>`;
+        chatbox.innerHTML += messageHTML;
+        chatbox.scrollTop = chatbox.scrollHeight;
 
-    const response = await fetch('/chat', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ message: message })
-    });
+        const response = await fetch('/chat', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message: message })
+        });
 
-    const data = await response.json();
-    chatbox.innerHTML += `<div class="message bot-message">${data.response}</div>`;
+        const data = await response.json();
+        chatbox.innerHTML += `<div class="message bot-message">${data.response}</div></div>`;
+        chatbox.scrollTop = chatbox.scrollHeight;
+    }
 }
